fae7527a10891354934a408f3c0b5b6673ca447f
@BagliettoAron <aronbaglietto88@gmail.com>
2022-05-20 08:29:04 +0200

    Add: axios, bootsrap, AlbumList & WIP AlbumCard

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="album">
+    <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+    <p>New Jersey</p>
+    <p>Bon Jovi</p>
+    <p>1988</p>
+
+
+</div>
+  
+</template>
+
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+
+<style>
+@import "../style/variables.scss"
+
+</style>
---
+++ src/components/AlbumList.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="album-list row w-75 row-cols-5 ">
+    <AlbumCard />
+    <AlbumCard />
+    <AlbumCard />
+    <AlbumCard />
+    <AlbumCard />
+    <AlbumCard />
+    <AlbumCard />
+    <AlbumCard />
+    <AlbumCard />
+    <AlbumCard />
+  </section>
+</template>
+
+<script>
+import AlbumCard from "./AlbumCard.vue"
+import axios from "axios";
+
+export default {
+  name: "AlbumList",
+
+  components: {
+    AlbumCard,
+  },
+
+  data: function () {
+    return {
+      albumsArray: [],
+    };
+  },
+
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((resp) => {
+        this.albumsArray = resp.data.response;
+      });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+@import "../style/common.scss"
+
+
+</style>
